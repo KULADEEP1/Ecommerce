43ff4ed47a68344827c2ed7e61d7ecd954665ee2
KULADEEP1 <basettykuladeep@gmail.com>
2024-05-22 07:32:24 +0530

    new commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 
 const API_BASE_URL = "http://localhost:5000";
+// const API_BASE_URL = "https://blog-mern-backend-ua18.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
